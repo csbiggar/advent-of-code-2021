8289fa8624c69303a0205beffbaddc145bc0d246
Carolyn Biggar <carolyn.biggar@johnlewis.co.uk>
2021-12-03 23:23:24 +0000

    Day 3 part 2

--- src/main/kotlin/days/Day3.kt
+++ src/main/kotlin/days/Day3.kt
@@ -9,16 +9,18 @@ private const val DELIMITER = " "
 fun main() {
     val input = FileReader("/day3.txt").readStrings()
 
-    val resultPart1 = Day3().getPowerConsumption(input)
+    val resultPart1 = Day3().calculatePowerConsumption(input)
     println("Part 1: $resultPart1")
-//
-//    val resultPart2 = Day3().withAim(input)
-//    println("Part 2: $resultPart2")
+
+    val resultPart2 = Day3().calculateLifeSupportRating(input)
+    println("Part 2: $resultPart2")
 }
 
 
 class Day3 {
-    fun getPowerConsumption(input: List<String>): Long {
+
+    // Part 1
+    fun calculatePowerConsumption(input: List<String>): Long {
         val gamma = findGammaRate(input)
         val epsilon = invertBinary(gamma)
 
@@ -30,8 +32,7 @@ class Day3 {
 
         val binary = (0..finalIndex)
             .map { bitIndex ->
-                val atThisIndex = input.map { binaryNumber -> binaryNumber[bitIndex] }
-                mostCommon(atThisIndex)
+                input.getMostCommonAtIndex(bitIndex)
             }
             .joinToString("")
 
@@ -44,14 +45,64 @@ class Day3 {
             .replace('x', '1')
     }
 
+    // Part 2
+    fun calculateLifeSupportRating(input: List<String>): Long {
+        val o2Generator = findO2GeneratorRating(input)
+        val co2Scrubber = findCO2ScrubberRating(input)
+        return o2Generator.toLong(2) * co2Scrubber.toLong(2)
+    }
+
+    fun findO2GeneratorRating(input: List<String>): String {
+        return filterMostCommon(input)
+    }
+
+    fun findCO2ScrubberRating(input: List<String>): String {
+        return filterLeastCommon(input)
+    }
+
+    // Shared - refactor to combine most / least common
+    private tailrec fun filterMostCommon(input: List<String>, bitIndex: Int = 0): String {
+        val finalIndex = input.first().count() - 1
+        if (bitIndex > finalIndex) throw IllegalStateException("Arg, too much loopiness")
+
+        val mostCommonBitAtIndex = input.getMostCommonAtIndex(bitIndex)
+        val filtered = input.filter { it[bitIndex] == mostCommonBitAtIndex }
+
+        return when (filtered.size) {
+            0 -> throw IllegalStateException("This can't be right, everything has been filtered out")
+            1 -> filtered.first()
+            else -> filterMostCommon(filtered, bitIndex + 1)
+        }
+    }
+
+    private tailrec fun filterLeastCommon(input: List<String>, bitIndex: Int = 0): String {
+        val finalIndex = input.first().count() - 1
+        if (bitIndex > finalIndex) throw IllegalStateException("Arg, too much loopiness")
+
+        val leastCommonBitAtIndex = input.getLeastCommonAtIndex(bitIndex)
+        val filtered = input.filter { it[bitIndex] == leastCommonBitAtIndex }
+
+        return when (filtered.size) {
+            0 -> throw IllegalStateException("This can't be right, everything has been filtered out")
+            1 -> filtered.first()
+            else -> filterLeastCommon(filtered, bitIndex + 1)
+        }
+    }
+
+    private fun List<String>.getMostCommonAtIndex(bitIndex: Int): Char {
+        val bitValuesAtThisIndex = map { binaryNumber -> binaryNumber[bitIndex] }
+        return mostCommon(bitValuesAtThisIndex)
+    }
+
+    private fun List<String>.getLeastCommonAtIndex(bitIndex: Int): Char {
+        val bitValuesAtThisIndex = map { binaryNumber -> binaryNumber[bitIndex] }
+        return leastCommon(bitValuesAtThisIndex)
+    }
+
+
+    // Move this out
     fun mostCommon(bits: List<Char>): Char {
-        val bitValueCount: Map<Char, Int> = bits
-            .groupBy { it }
-            .mapValues { (bitValue, bits) -> bits.size }
-
-        val zeros = bitValueCount.getOrDefault('0', 0)
-        val ones = bitValueCount.getOrDefault('1', 0)
-
+        val (zeros, ones) = bitCounts(bits)
         return when {
             zeros > ones -> '0'
             else -> '1'
@@ -59,5 +110,27 @@ class Day3 {
 
     }
 
+    fun leastCommon(bits: List<Char>): Char {
+        val (zeros, ones) = bitCounts(bits)
+        return when {
+            zeros <= ones -> '0'
+            else -> '1'
+        }
 
+    }
+
+    private fun bitCounts(bits: List<Char>): BitCounts {
+        val bitValueCount: Map<Char, Int> = bits
+            .groupBy { it }
+            .mapValues { (bitValue, bits) -> bits.size }
+
+        val zeros = bitValueCount.getOrDefault('0', 0)
+        val ones = bitValueCount.getOrDefault('1', 0)
+        return BitCounts(zeros = zeros, ones = ones)
+    }
 }
+
+data class BitCounts(
+    val zeros: Int,
+    val ones: Int
+)
--- src/test/kotlin/days/Day3Test.kt
+++ src/test/kotlin/days/Day3Test.kt
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Test
 
 internal class Day3Test {
 
+
     @Test
     fun `part 1`() {
-        val result = Day3().getPowerConsumption(input)
+        val result = Day3().calculatePowerConsumption(input)
         assertThat(result).isEqualTo(198)
     }
 
@@ -29,9 +30,32 @@ internal class Day3Test {
     }
 
     @Test
+    fun `should find the least common digit in a list of 0s and 1s`() {
+        val input1 = listOf('1', '0', '1')
+        val input2 = listOf('1', '0', '0')
+        val input3 = listOf('1', '0', '0', '1')
+
+        assertThat(Day3().leastCommon(input1)).`as`("mostly ones").isEqualTo('0')
+        assertThat(Day3().leastCommon(input2)).`as`("mostly zeros").isEqualTo('1')
+        assertThat(Day3().leastCommon(input3)).`as`("same number").isEqualTo('0')
+    }
+
+    @Test
+    fun `should find the binary number that represents the generator rating`() {
+        val result: String = Day3().findO2GeneratorRating(input)
+        assertThat(result).isEqualTo("10111")
+    }
+
+    @Test
+    fun `should find the binary number that represents the scrubber rating`() {
+        val result: String = Day3().findCO2ScrubberRating(input)
+        assertThat(result).isEqualTo("01010")
+    }
+
+    @Test
     fun `part 2`() {
-//        val result = Day3().getPowerConsumption(input)
-//        assertThat(result).isEqualTo(198)
+        val result = Day3().calculateLifeSupportRating(input)
+        assertThat(result).isEqualTo(230)
     }
 
     private val input = listOf(
